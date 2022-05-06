2ffd3a5880f2987726b976f8b1e8fb595983ecaf
JOHN998ERICK <57510458+JOHN998ERICK@users.noreply.github.com>
2022-05-05 23:20:11 -0500

    Add files via upload

---
+++ abrilservicio1/src/main/java/com/idat/AbrilServicioI/modelo/Productos.java
@@ -0,0 +1,62 @@
+package com.idat.AbrilServicioI.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "Productos")
+@Entity
+public class Productos implements Serializable{
+	
+	
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idProducto;
+	private String nombreProducto;
+	private String descripcion;
+	private Double precio;
+	private Integer stock;
+	
+	
+	public Productos() {
+		super();
+	}
+	public Integer getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(Integer idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Integer getStock() {
+		return stock;
+	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+	
+	
+}
